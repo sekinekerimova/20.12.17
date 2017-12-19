48f6839ed26a7a3019ffac57b24bd93a372e364b
sekinekerimova <34401656+sekinekerimova@users.noreply.github.com>
2017-12-20 00:33:23 +0400

    Add files via upload

---
+++ RepeatTime.js
@@ -0,0 +1,40 @@
+var eded1=Number(prompt("ededi daxil edin"));
+var reqem=0;
+var say=0;
+while(eded1>0){var cem=0;
+    var eded2=eded1;
+
+while(eded2>0) {
+    reqem=eded2%10;
+    cem=cem+reqem;
+    eded2=parseInt( eded2/10);
+    
+}
+eded1=eded1-cem;
+say++;
+console.log( eded1,cem,say);
+console.log("repeat="+say);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
